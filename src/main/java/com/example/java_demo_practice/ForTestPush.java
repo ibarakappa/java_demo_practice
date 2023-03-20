b37d35f8fc75b80b9612d1a08dfaa93879f53b69
DESKTOP-RRPRPE7\Lenovo <yz1410aa@gmail.com>
2023-03-20 14:11:35 +0800

    try push

---
+++ src/main/java/com/example/java_demo_practice/ForTestPush.java
@@ -0,0 +1,12 @@
+package com.example.java_demo_practice;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForTestPush {
+
+	public static void main(String[] args) {
+		System.out.println("´ú¸Õpush");
+	}
+
+}
