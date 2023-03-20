79d45c639104f8ec17b73a9a635f84c1fea11903
DESKTOP-RRPRPE7\Lenovo <yz1410aa@gmail.com>
2023-03-20 12:17:53 +0800

    commmit gradle

---
+++ src/main/java/com/example/java_demo_practice/JavaDemoPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_demo_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDemoPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDemoPracticeApplication.class, args);
+	}
+
+}
